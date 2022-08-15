adfd0c27a9475c8f125ed58fa65a6a90642ca1d3
Anton <dragon321001@gmail.com>
2022-08-16 01:22:23 +0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+const closeElem = document.querySelector('.menu__close');
+
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+const counters = document.querySelectorAll('.skills__progress-counter');
+const lines = document.querySelectorAll('.skills__progress-line span');
+
+counters.forEach( (item,i) => {
+    lines[i].style.width = item.innerHTML;
+});
